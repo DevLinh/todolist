1d5d91d145ca95fbaf428d94903e3da43bb04ee4
DevLinh <devlinh99qb@gmail.com>
2018-11-17 22:02:46 +0700

    Updates

---
+++ precache-manifest.541ca20c897779d86ada4acc05bc88b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f2982e14ddd10bc74a84",
+    "url": "/todolist/static/js/runtime~main.f2982e14.js"
+  },
+  {
+    "revision": "a06ac4d70633f163202c",
+    "url": "/todolist/static/js/main.a06ac4d7.chunk.js"
+  },
+  {
+    "revision": "9a9a74dd771085c67093",
+    "url": "/todolist/static/js/1.9a9a74dd.chunk.js"
+  },
+  {
+    "revision": "a06ac4d70633f163202c",
+    "url": "/todolist/static/css/main.8a8501ea.chunk.css"
+  },
+  {
+    "revision": "b67c115ef878dfef77ab0db3ef0c1aff",
+    "url": "/todolist/index.html"
+  }
+];
